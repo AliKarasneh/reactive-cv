a81f2b0258cdba53561b5e0ace636ea94f2be76c
alikarasneh <ali_karasneh@hotmail.com>
2021-06-24 07:20:53 +0300

    initial comit

---
+++ src/templates/blocks/Icons.js
@@ -0,0 +1,38 @@
+import {
+  FaBehance,
+  FaBirthdayCake,
+  FaDribbble,
+  FaFacebookF,
+  FaGithub,
+  FaGitlab,
+  FaGlobeAmericas,
+  FaInstagram,
+  FaLinkedinIn,
+  FaSkype,
+  FaStackOverflow,
+  FaTelegram,
+  FaYoutube,
+  FaTwitter,
+} from 'react-icons/fa';
+import { MdEmail, MdPhone } from 'react-icons/md';
+
+const Icons = {
+  phone: MdPhone,
+  website: FaGlobeAmericas,
+  email: MdEmail,
+  facebook: FaFacebookF,
+  twitter: FaTwitter,
+  linkedin: FaLinkedinIn,
+  github: FaGithub,
+  dribbble: FaDribbble,
+  instagram: FaInstagram,
+  stackoverflow: FaStackOverflow,
+  behance: FaBehance,
+  gitlab: FaGitlab,
+  birthday: FaBirthdayCake,
+  telegram: FaTelegram,
+  skype: FaSkype,
+  youtube: FaYoutube,
+};
+
+export default Icons;
